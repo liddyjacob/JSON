8c3a42c11f59e444e409097c3af20aad27fddc95
Jacob Liddy <liddyjacob@gmail.com>
2017-11-12 21:21:52 -0500

    Completed object class

--- json.hpp
+++ json.hpp
@@ -4,33 +4,79 @@
 #pragma once
 
 #include <vector>
+#include <map>
 
-
-struct Object{
-
-
-	std::string name;
-	Value val;
-};
-
+#include <iostream>
 
 struct  Value{
 
 	virtual void print() = 0;
 
-	virtual ~Value = default;
+	virtual ~Value() = default; 
 };
+
 
 struct Array : Value{
 
 	Array(std::vector<Value*> vals)
-		: vals(vals);
+		: vals(vals)
 	{ }
+
+	void print() override {
+		std::cout << '[';
+		for (auto iter = vals.begin(); iter != vals.end(); ++iter){
+			(*iter)->print();
+			if (std::next(iter) != vals.end())
+				std::cout << ", ";
+		}
+		std::cout << ']';
+	}
+	
+	~Array() override {
+		for (val : vals)
+			delete val;
+	}
 
 	std::vector<Value*> vals;
 
-	~Array()
-}
+	
+};
+
+struct String : Value {
 
 
-struct 
+
+};
+
+ 
+struct Object{
+
+	Object() { }
+
+	~Object() { 
+		for (auto p : map){
+			delete p.first;
+			delete p.second;
+		}
+	}
+
+	void print(){
+		std::cout << '{';
+		for ( auto iter = map.begin(); iter!= map.end(); ++iter){
+			iter->first->print();
+			std::cout << " : ";
+			iter->second->print();
+			if (std::next(iter)!= map.end())
+				std::cout << ", ";
+		}
+		std::cout << "}";
+	}
+
+	void add(String* nm, Value* v){
+		map.emplace(nm, v);
+	}
+
+	std::map<String*, Value*> map;
+};
+
+
---
+++ test.cpp
@@ -0,0 +1,8 @@
+///test.cpp - Demonstrait some basic functionallity
+// of the json implimentation - specifically print to
+// a file. 
+
+int main(){
+
+	return 0;
+}
